b6ccf5ce4b8bae29db330ad4b6f6d50eeb1aae89
Matt Zanchelli <matt@mdznr.com>
2013-05-16 00:03:16 -0400

    Initial Commit

---
+++ Handedness/MTZAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  MTZAppDelegate.h
+//  Handedness
+//
+//  Created by Matt on 5/16/13.
+//  Copyright (c) 2013 Matt Zanchelli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MTZViewController;
+
+@interface MTZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) MTZViewController *viewController;
+
+@end
---
+++ Handedness/MTZViewController.m
@@ -0,0 +1,29 @@
+//
+//  MTZViewController.m
+//  Handedness
+//
+//  Created by Matt on 5/16/13.
+//  Copyright (c) 2013 Matt Zanchelli. All rights reserved.
+//
+
+#import "MTZViewController.h"
+
+@interface MTZViewController ()
+
+@end
+
+@implementation MTZViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
